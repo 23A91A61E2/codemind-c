967ddb373b47288af7d532e33b0cdecab0a3995f
23A91A61E2 <147410265+23A91A61E2@users.noreply.github.com>
2023-10-18 09:39:05 +0530

    #include<stdio.h> int main() {     int a,area,perimeter;     scanf("%d",&a);     area=(a*a);     perimeter=(4*a);     printf("%d %d",area,perimeter); }

--- Area_and_Perimeter_of_Square.c
+++ Area_and_Perimeter_of_Square.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int a,area, perimeter;
+    int a,area,perimeter;
     scanf("%d",&a);
     area=(a*a);
     perimeter=(4*a);
-    printf("%d %d",area, perimeter);
+    printf("%d %d",area,perimeter);
 }
